f26f9ae83b4a033af26a093988a9299bcfb1162f
LEIYAO123 <lei@rushable.io>
2025-03-25 19:07:22 +0800

    feat(orders): 添加订单列表的下拉刷新和加载更多功能- 在订单列表中集成 PullToRefreshBox 组件，实现下拉刷新功能 - 添加加载更多功能，在列表底部显示加载中指示器 - 优化列表滚动逻辑，自动检测并触发加载更多 - 调整页面布局，以适应新的刷新和加载更多功能

--- app/src/main/java/com/quick/app/pages/orders/index.kt
+++ app/src/main/java/com/quick/app/pages/orders/index.kt
@@ -3,20 +3,37 @@ package com.quick.app.pages.orders
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.quick.app.components.RuTopAppBar
 import com.quick.app.pages.orders.comps.OrderItem
 import com.quick.app.pages.orders.comps.OrderTab
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun OrdersRoute() {
@@ -28,6 +45,36 @@ fun OrdersRoute() {
 fun OrdersScreen(vm: OrdersViewModel = viewModel()) {
     val listState = rememberLazyListState()
 
+    val state = rememberPullToRefreshState()
+    var isRefreshing by remember { mutableStateOf(false) }
+    var isLoadMore by remember { mutableStateOf(false) }
+
+    val coroutineScope = rememberCoroutineScope()
+    val onRefresh: () -> Unit = {
+        isRefreshing = true
+        coroutineScope.launch {
+            delay(5000)
+            isRefreshing = false
+        }
+    }
+
+    // 检测是否到达底部，如果是，触发加载更多
+    val isAtBottom = remember {
+        derivedStateOf {
+            val layoutInfo = listState.layoutInfo
+            layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
+        }
+    }
+
+    // 当到达底部时触发加载更多
+    LaunchedEffect(isAtBottom.value) {
+        if (isAtBottom.value) {
+            isLoadMore = true
+            delay(5000)
+            isLoadMore = false
+        }
+    }
+
     Scaffold(topBar = { RuTopAppBar("订单列表", toBack = true) }) { pv ->
         Column(
             Modifier
@@ -38,19 +85,49 @@ fun OrdersScreen(vm: OrdersViewModel = viewModel()) {
                 selectedIndex = vm.currIndex.intValue,
                 tabChanged = vm::handleSwitchType,
             )
-            LazyColumn(
-                state = listState,
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .padding(vertical = 6.dp, horizontal = 12.dp)
-            ) {
-                items(vm.list.value.size) {
-                    OrderItem(
-                        data = vm.list.value[it],
-                        modifier = Modifier.clickable {}
+            PullToRefreshBox(
+                isRefreshing = isRefreshing,
+                state = state,
+                indicator = {
+                    Indicator(
+                        modifier = Modifier.align(Alignment.TopCenter),
+                        isRefreshing = isRefreshing,
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        state = state
                     )
+                },
+                onRefresh = onRefresh,
+                modifier = Modifier.weight(1f)
+            ) {
+                LazyColumn(
+                    state = listState,
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 6.dp, horizontal = 12.dp)
+                ) {
+                    items(vm.list.value.size) {
+                        OrderItem(
+                            data = vm.list.value[it],
+                            modifier = Modifier.clickable {}
+                        )
+                    }
+                    // 加载更多的提示
+                    if (isLoadMore) {
+                        item {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                CircularProgressIndicator(
+                                    modifier = Modifier
+                                        .padding(4.dp)
+                                        .size(24.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
